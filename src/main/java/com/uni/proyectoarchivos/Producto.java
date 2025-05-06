13c819155555b770f0741f7f443032a24c6350a8
SARA <cancinosara39@gmail.com>
2025-05-05 21:58:16 -0600

    Proyecto archivos

---
+++ src/main/java/com/uni/proyectoarchivos/Producto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uni.proyectoarchivos;
+
+/**
+ *
+ * @author daphne
+ */
+public class Producto {
+    public int Clave;
+    public String Nombre;
+    public int Cantidad;
+    public double Precio;
+    public String Fecha;
+
+    
+}
